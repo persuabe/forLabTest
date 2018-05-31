b5acde862c67af17ef3ab19669073a0d2582c9cc
persuabe <persuabe@gmail.com>
2018-06-01 04:39:23 +0700

    update python file

--- one.py
+++ one.py
@@ -4,29 +4,23 @@
 class Flower:
     def __init__(self, name, cost):
         self.name = name
-
         self.cost = cost
-
-
 
     def printinfo(self):
         print(self.name, " стоит ", self.cost)
 
 
 class IteratorFlower:
-
     def iterateFlower(self, count):
         list = ["Роза", "Тюльпан", "Пион", "Орхидея"]
-
         for i in list:
             f = Flower(i, random.randint(40, 70))
             f.printinfo()
 
 
 def main():
-    iterator = IteratorFlower();
-
-    iterator.iterateFlower(10);
+    iterator = IteratorFlower()
+    iterator.iterateFlower(10)
 
 
 if __name__ == "__main__":
